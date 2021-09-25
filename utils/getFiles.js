1301558f49fad62bef90bcbaeb202193f11c3f91
Victor Tesoura Júnior <txsoura@yahoo.com>
2021-09-25 18:38:15 -0300

    feat(utils): add laravel migration name formatter

--- utils/getFiles.js
+++ utils/getFiles.js
@@ -21,6 +21,15 @@ async function* makeFileTemplate(frameworkDirectory, name) {
   const namePlural = pluralize(name)
 
   for await (const f of getFiles(frameworkDirectory)) {
+    const date = new Date()
+    const hour = date
+      .toISOString()
+      .split('T')[1]
+      .replace(/:/gi, '')
+      .split('.')[0]
+    const fullDate = date.toISOString().split('T')[0].replace(/-/gi, '_')
+    const migrationName = `${fullDate}_${hour}_create_${namePlural.toLowerCase()}`
+
     yield {
       src: f.replace(frameworkDirectory, ''),
       dist: f
@@ -28,7 +37,9 @@ async function* makeFileTemplate(frameworkDirectory, name) {
         .replace('.txt', '')
         .replace('__name__', name)
         .replace('__name_plural__', namePlural)
-        .replace('__name_low__', name.toLowerCase()),
+        .replace('__name_low__', name.toLowerCase())
+        .replace(`__migration_plural_low__`, migrationName)
+        .replace('__name_plural_low__', namePlural.toLowerCase()),
     }
   }
 }
